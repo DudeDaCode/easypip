f8bd315d64dd12d2deffdbd22e3d7cb2f097fc88
DudeDaCode <68649648+DudeDaCode@users.noreply.github.com>
2020-07-30 19:14:56 +0530

    Version 1.2

--- easypip.py
+++ easypip.py
@@ -5,31 +5,50 @@ root= tk.Tk()
 
 root.wm_title("easypip")
 
-canvas1 = tk.Canvas(root, width = 350, height = 100, bg = '#FFD43B', relief = 'raised')
+canvas1 = tk.Canvas(root, width = 200, height = 400, bg = '#FFD43B', relief = 'raised')
 canvas1.pack()
 
+label1 = tk.Label(root, text='easypip v1.2', bg = '#FFD43B')
+label1.config(font=('calibri', 10))
+canvas1.create_window(100, 25, window=label1)
+
 entry1 = tk.Entry (root) 
-canvas1.create_window(175, 25, window=entry1)
+canvas1.create_window(100, 50, window=entry1)
 
 def installPackage ():
     package = entry1.get()
     os.system(f'start cmd /k pip install {package}')
 
-def installFromRepo ():
+def installFromVCS ():
     link = entry1.get()
-    os.system(f'start cmd /k python -m pip install git+{link}')
+    os.system(f'start cmd /k python -m pip install {link}')
 
 def installFromRequirements ():
-    os.system('pip install -r requirements.txt')
+    textfile = entry1.get()
+    os.system(f'start cmd /k pip install -r {textfile}')
+
+def installFromFile ():
+    directory = entry1.get()
+    os.system(f'start cmd /k pip install {directory}')
+
+def installFromSource ():
+    sourcedir = entry1.get()
+    os.system(f'start cmd /k pip install {sourcedir}')
     
 button1 = tk.Button(text='      Install      ', command=installPackage, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(65, 75, window=button1)
+canvas1.create_window(100, 100, window=button1)
 
-button2 = tk.Button(text='      Link     ', command=installFromRepo, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(180, 75, window=button2)
+button2 = tk.Button(text='      Link     ', command=installFromVCS, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(100, 150, window=button2)
 
 button3 = tk.Button(text='      Text     ', command=installFromRequirements, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(295, 75, window=button3)
+canvas1.create_window(100, 200, window=button3)
+
+button4 = tk.Button(text='      From Archive     ', command=installFromFile, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(100, 250, window=button4)
+
+button4 = tk.Button(text='      From Source     ', command=installFromSource, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(100, 300, window=button4)
 
 
 root.mainloop()
