2afcf0f6a48400f248791d97072eaae3f4b49cda
DudeDaCode <68649648+DudeDaCode@users.noreply.github.com>
2020-07-30 19:44:57 +0530

    Version 1.4

--- easypip.py
+++ easypip.py
@@ -5,10 +5,10 @@ root= tk.Tk()
 
 root.wm_title("easypip")
 
-canvas1 = tk.Canvas(root, width = 200, height = 400, bg = '#FFD43B', relief = 'raised')
+canvas1 = tk.Canvas(root, width = 200, height = 450, bg = '#FFD43B', relief = 'raised')
 canvas1.pack()
 
-label1 = tk.Label(root, text='easypip v1.2', bg = '#FFD43B')
+label1 = tk.Label(root, text='easypip v1.4', bg = '#FFD43B')
 label1.config(font=('calibri', 10))
 canvas1.create_window(100, 25, window=label1)
 
@@ -34,6 +34,17 @@ def installFromFile ():
 def installFromSource ():
     sourcedir = entry1.get()
     os.system(f'start cmd /k pip install {sourcedir}')
+
+def makePackage ():
+    packagedir = entry1.get()
+    os.chdir(f'{packagedir}')
+    os.system(f'start cmd /k python setup.py sdist bdist_wheel')
+
+def upgradePackage ():
+    upackage = entry1.get()
+    os.system(f'start cmd /k pip install --upgrade {upackage}')
+
+
     
 button1 = tk.Button(text='      Install      ', command=installPackage, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(100, 100, window=button1)
@@ -47,8 +58,14 @@ canvas1.create_window(100, 200, window=button3)
 button4 = tk.Button(text='      From Archive     ', command=installFromFile, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(100, 250, window=button4)
 
-button4 = tk.Button(text='      From Source     ', command=installFromSource, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(100, 300, window=button4)
+button5 = tk.Button(text='      From Source     ', command=installFromSource, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(100, 300, window=button5)
+
+button6 = tk.Button(text='      Make Package     ', command=makePackage, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(100, 350, window=button6)
+
+button7 = tk.Button(text='      Upgrade     ', command=upgradePackage, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(100, 400, window=button7)
 
 
 root.mainloop()
