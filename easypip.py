aa72f89526c22432a6cf3e407a68687a902aeb70
Darren the Barren <68649648+csharpisez@users.noreply.github.com>
2020-11-25 12:17:43 +0530

    v2.0
    now uses PySimpleGUI and doesnt look like my mom

--- easypip.py
+++ easypip.py
@@ -1,71 +1,63 @@
+import PySimpleGUI as sg
 import os
-import tkinter as tk
 
-root= tk.Tk()
+sg.theme('Reddit')  # Add some color to the window
 
-root.wm_title("easypip")
+layout = [
+    [sg.InputText('', size=(37,1), key='input_value')],
+    [sg.InputOptionMenu(('Install Package', 'Install From VCS', 'Install From Requirements', 'Install From File', 'Install From Source',
+                         'Make Package', 'Upgrade Package'), size=(32,1), key='-OPTION-')],
+    [sg.Submit(size=(32,1))],]
 
-canvas1 = tk.Canvas(root, width = 200, height = 450, bg = '#FFD43B', relief = 'raised')
-canvas1.pack()
-
-label1 = tk.Label(root, text='easypip v1.4', bg = '#FFD43B')
-label1.config(font=('calibri', 10))
-canvas1.create_window(100, 25, window=label1)
-
-entry1 = tk.Entry (root) 
-canvas1.create_window(100, 50, window=entry1)
-
+window = sg.Window('Easy Pip', layout)
+event, values = window.read()
+    
 def installPackage ():
-    package = entry1.get()
-    os.system(f'start cmd /k pip install {package}')
+    os.system(f'start cmd /k pip install {value}')
 
 def installFromVCS ():
-    link = entry1.get()
-    os.system(f'start cmd /k python -m pip install {link}')
+    os.system(f'start cmd /k python -m pip install {value}')
 
 def installFromRequirements ():
-    textfile = entry1.get()
-    os.system(f'start cmd /k pip install -r {textfile}')
+    os.system(f'start cmd /k pip install -r {value}')
 
 def installFromFile ():
-    directory = entry1.get()
-    os.system(f'start cmd /k pip install {directory}')
+    os.system(f'start cmd /k pip install {value}')
 
 def installFromSource ():
-    sourcedir = entry1.get()
-    os.system(f'start cmd /k pip install {sourcedir}')
+    os.system(f'start cmd /k pip install {value}')
 
 def makePackage ():
-    packagedir = entry1.get()
-    os.chdir(f'{packagedir}')
+    os.chdir(f'{value}')
     os.system(f'start cmd /k python setup.py sdist bdist_wheel')
 
 def upgradePackage ():
-    upackage = entry1.get()
-    os.system(f'start cmd /k pip install --upgrade {upackage}')
+    os.system(f'start cmd /k pip install --upgrade {value}')
+
+if event == 'Submit':
+    value = values['input_value']
+    option = values['-OPTION-']
+
+if option == 'Install Package' :
+    installPackage()
+
+if option == 'Install From VCS' :
+    installFromVCS()
+
+if option == 'Install From File' :
+    installFromFile()
+
+if option == 'Install From Requirements' :
+    installFromRequirements()
+
+if option == 'Install From Source' :
+    installFromSource()
+
+if option == 'Make Package' :
+    makePackage()
+
+if option == 'Upgrade Package' :
+    upgradePackage()
 
 
-    
-button1 = tk.Button(text='      Install      ', command=installPackage, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(100, 100, window=button1)
-
-button2 = tk.Button(text='      Link     ', command=installFromVCS, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(100, 150, window=button2)
-
-button3 = tk.Button(text='      Text     ', command=installFromRequirements, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(100, 200, window=button3)
-
-button4 = tk.Button(text='      From Archive     ', command=installFromFile, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(100, 250, window=button4)
-
-button5 = tk.Button(text='      From Source     ', command=installFromSource, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(100, 300, window=button5)
-
-button6 = tk.Button(text='      Make Package     ', command=makePackage, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(100, 350, window=button6)
-
-button7 = tk.Button(text='      Upgrade     ', command=upgradePackage, bg='#306998', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(100, 400, window=button7)
-
-
-root.mainloop()
+window.close()
